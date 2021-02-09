8029ec8bc6877de864796ea15b8d9e9303a78e33
Melissa Wong <melissarwong@berkeley.edu>
2021-02-08 21:11:32 -0800

    sp21/lab/lab04.push

---
+++ materials/sp21/lab/lab04/tests/q11.py
@@ -0,0 +1,32 @@
+test = {
+  'name': 'q11',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Make sure your function has the proper syntax!;
+          >>> to_percentage(.35) == 35.0
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> # Make sure you are passing in the proper argument!;
+          >>> twenty_percent == 20.0
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
