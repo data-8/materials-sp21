1f31f78b20bf2ad190dd058ac4c6188eb1eac76d
Melissa Wong <melissarwong@berkeley.edu>
2021-04-05 21:53:21 -0700

    sp21/lab/lab08.push

---
+++ materials/sp21/lab/lab08/tests/q2_8.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q2_8',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> q2_8 == 1
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
