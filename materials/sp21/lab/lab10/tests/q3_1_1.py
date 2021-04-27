4a9196e10632b41fe159c0c72e84eeac4b8688ff
Melissa Wong <melissarwong@berkeley.edu>
2021-04-26 21:19:12 -0700

    sp21/lab/lab10.push

---
+++ materials/sp21/lab/lab10/tests/q3_1_1.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'q3_1_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> cancer
+          status    | negative | positive
+          cancer    | 10       | 90
+          no cancer | 9702     | 198
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
